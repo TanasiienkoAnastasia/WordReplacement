973171b79270225d467cced467f65559de3f0246
TanasienkoAnastasiia <tanasiienkoanastasia@gmail.com>
2023-05-13 00:17:30 +0300

    word replacement program

---
+++ Main.py
@@ -0,0 +1,7 @@
+def replace_word():
+    str = "Hi, guys, I am Nastya"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Input the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
